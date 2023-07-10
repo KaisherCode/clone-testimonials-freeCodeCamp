e69544fb02ad7a8b7e7e3cb9ccac165233721e12
Wilson Dev <wilson.hch@hynkor.com>
2023-07-10 15:21:03 -0500

    Crear componente testimonio

---
+++ src/componentes/testimonial.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+function Testimonial (){
+  return(
+    <div className="container-testimonial">
+      <img className="image-testimonial" src={require("../images/Emma-Bostian")} alt="Foto de Emma"/>
+    </div>
+  )
+}
