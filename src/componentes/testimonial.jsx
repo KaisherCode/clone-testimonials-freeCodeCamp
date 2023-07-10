8b58572cd81bd597e5c35ac5a3e49119bde006d4
Wilson Dev <wilson.hch@hynkor.com>
2023-07-10 17:55:32 -0500

    Recibir y pasar props y personalizar imagen y texto en negritan

--- src/componentes/testimonial.jsx
+++ src/componentes/testimonial.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import imagesEmma from  "../images/Emma-Bostian.png";
+
 import "../styles/Testimonial.css";
 
-function Testimonial() {
+function Testimonial(props) {
   return (
     <div className="container-testimonial">
-      <img className="image-testimonial" src={imagesEmma} alt="Foto de Emma" />
+      <img className="image-testimonial" src={require(`../images/${props.image}.png`)} alt="Foto de Emma" />
       <div className="container-text-testimonial">
-        <p className="name-testimonial">Emma Bostian in Sweden</p>
-        <p className="job-testimonial">Software Engineer at Spotify</p>
+        <p className="name-testimonial"> <strong> {props.name}</strong> in {props.country}</p>
+        <p className="job-testimonial">{props.jobs} at <strong>{props.company}</strong> </p>
         <p className="text-testimonial">
-          "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+          "{props.testimonial}"
         </p>
       </div>
     </div>
   )
 }
-export {Testimonial}
+export { Testimonial }
