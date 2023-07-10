5bca2174b9a37731ece13f5fcb8fb09c2db49a34
Wilson Dev <wilson.hch@hynkor.com>
2023-07-10 15:50:36 -0500

    Mostrar componente testimonial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
+import { Testimonial } from './componentes/testimonial';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Testimonial/>
     </div>
   );
 }
-
 export default App;
