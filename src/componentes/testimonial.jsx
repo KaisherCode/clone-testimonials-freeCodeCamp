3235b22bb32fd6a7617ff433fd279bbb6f550462
Wilson Dev <wilson.hch@hynkor.com>
2023-07-10 15:37:46 -0500

    Maquetación del componente testimonial

--- src/componentes/testimonial.jsx
+++ src/componentes/testimonial.jsx
@@ -5,6 +5,14 @@ function Testimonial (){
   return(
     <div className="container-testimonial">
       <img className="image-testimonial" src={require("../images/Emma-Bostian")} alt="Foto de Emma"/>
+      <div className="container-text-testimonial">
+      <p className="name-testimonial">Emma Bostian in Sweden</p>
+      <p className="job-testimonial">Software Engineer at Spotify</p>
+      <p className="text-testimonial">
+      "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+      </p>
+
+      </div>
     </div>
   )
 }
