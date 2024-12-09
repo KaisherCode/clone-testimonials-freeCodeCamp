910606e4d65a5981ef4179dbeb3aabb305a318c0
Wilson Dev <wilson.hch@hynkor.com>
2024-12-09 10:31:51 -0500

    fix(testimonial): Fix alt property to img tag

--- src/componentes/testimonial.jsx
+++ src/componentes/testimonial.jsx
@@ -5,7 +5,7 @@ import "../styles/Testimonial.css";
 function Testimonial(props) {
   return (
     <div className="container-testimonial">
-      <img className="image-testimonial" src={require(`../images/${props.image}.png`)} alt="Foto de Emma" />
+      <img className="image-testimonial" src={require(`../images/${props.image}.png`)} alt={props.name} />
       <div className="container-text-testimonial">
         <p className="name-testimonial"> <strong> {props.name}</strong> in {props.country}</p>
         <p className="job-testimonial">{props.jobs} at <strong>{props.company}</strong> </p>
